80d3d0a20e1f7671c00b7b6bf5475302163b9789
julio <juliorodriguezramirez@gmail.com>
2019-07-24 01:12:43 +0200

    Route Observables

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,17 +1,18 @@
 import { ActivatedRoute, Params } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubcription: Subscription;
 
   constructor(private route: ActivatedRoute) {
     // ActivatedRoute puede tener acceso al usuario seleccionado
-
    }
 
   ngOnInit() {
@@ -21,12 +22,14 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     };
     /*
-      Pudiendo trabajar con tareas asíncronas..
+      Pudiendo trabajar con tareas asíncronas...
       Los params de su ruta actualmente cargada podrían cambiar,
       pudiendo ocurrir en el futuro, para luego ejecutarlo.
       Asignándolo al objeto usuario.
+
+      -- No esta estrechamente ligado al componente, no se destruye.
     */
-    this.route.params
+    this.paramsSubcription = this.route.params
       .subscribe(
         (params: Params) => {
            this.user.id = params['id'];
@@ -34,5 +37,11 @@ export class UserComponent implements OnInit {
         }
       );
   }
+
+  ngOnDestroy(): void {
+    // Darte de baja manualmente.
+    this.paramsSubcription.unsubscribe;
+  }
+
 
 }
