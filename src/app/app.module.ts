fe300e4f9cc9c611fdacaf37b002e285620d85bd
Julio <juliorodriguezramirez@gmail.com>
2019-07-28 13:45:14 +0200

    Common Gotchas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersService } from "./servers/servers.service";
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent }, // localhost:...
-  { path: 'users', component: UserComponent }, // localhost:.../users
-  { path: 'users/:id/:name', component: UserComponent }, // Cargar dinámicamente una ruta
-  { path: 'servers', component: ServersComponent },
-  { path: 'servers/:id/edit', component: EditServerComponent } // Editar un servidor
-]
+  { path: "", component: HomeComponent }, // localhost:...
+  { path: "users", component: UserComponent }, // localhost:.../users
+  { path: "users/:id/:name", component: UserComponent }, // Cargar dinámicamente una ruta
+  { path: "servers", component: ServersComponent },
+  { path: "servers/:id", component: ServerComponent }, // Editar un servidor
+  { path: "servers/:id/edit", component: EditServerComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,12 +30,8 @@ const appRoutes: Routes = [
     EditServerComponent,
     ServerComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes)
-  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [ServersService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
